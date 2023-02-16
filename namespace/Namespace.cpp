925279a8e3c89fc9c8d7551488051a8e2ef2d27c
LeeHJ <lhjhoon@gmail.com>
2023-02-16 16:10:37 +0900

    test

--- namespace/Namespace.cpp
+++ namespace/Namespace.cpp
@@ -2,14 +2,12 @@
 
 #define TEXT "Welcome to C++ Programming!!"
 
- 
+// using namespace 를 사용하면 기존에 std::cout를 사용하던걸 std를 생략하고 사용할수 있음
+using namespace std;
 
 int main()
-
 {
-
-    std::cout << TEXT;
-
+    // std::cout << TEXT;
+    cout << TEXT;
     return 0;
-
 }
