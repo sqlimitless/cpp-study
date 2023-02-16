abff53cd87bc840e39179754af0c5f841ed9e440
LeeHJ <lhjhoon@gmail.com>
2023-02-16 16:12:18 +0900

    feat:iostream

--- iostream/Iostream.cpp
+++ iostream/Iostream.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main()
 {
     int age;
+    // << 삽입연산자
     cout << "여러분의 나이를 입력해 주세요 : ";
+    // >> 추출연산자
     cin >> age;
     cout << "여러분의 나이는 " << age << "살 입니다." << endl;
     return 0;
